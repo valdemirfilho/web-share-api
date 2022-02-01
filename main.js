28e0dd19c5dddd536653c11307dd4e1aa0e54016
Valdemir Filho <filhux@gmail.com>
2022-02-01 15:58:41 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const buttonShare = document.querySelector('#buttonshare')
+
+function shareText() {
+  console.log("click") 
+  
+  if (navigator.share) {
+    console.log('share api ok')
+
+    navigator.share({
+      title: 'web.dev',
+      text: 'Teste de compartilhamento de texto',
+      utl: 'https://web.dev/'
+    })
+      .then(() => console.log('Compartilhado com sucesso'))
+      .catch((error) => console.log('Error sharing', error));
+  }
+}
+
+buttonShare.addEventListener('click', shareText)
