b2d7fa7f6802addeb5515589173ab415d829406d
Valdemir Filho <filhux@gmail.com>
2022-02-02 20:26:08 -0300

    verify file share and text share

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ const buttonShare = document.querySelector('#buttonshare')
 async function shareTextAndImg() {
   console.log("click") 
   
-  const blob = await fetch('img/cat.jpg').then(r=>r.blob())
+  const response = await fetch('img/cat.jpg')
+  const blob = await response.blob()
   
   const file = new File([blob], "cat.jpg", {type: "image/jpeg"})
 
   const filesArray = [file]
   
   if (navigator.canShare && navigator.canShare({files: filesArray })) {
-    console.log('share api ok')
-
+    console.log('file share api ok')
     navigator.share({
       files: filesArray,
       title: 'web.dev',
@@ -20,6 +20,17 @@ async function shareTextAndImg() {
     })
       .then(() => console.log('Compartilhado com sucesso'))
       .catch((error) => console.log('Error sharing', error));
+
+  } else if (navigator.share) {
+    console.log('Share api OK')
+    navigator.share({
+      title: 'web.dev',
+      text: 'Teste de compartilhamento de texto',
+      url: 'https://pedalaki.tk'
+    })
+      .then(() => console.log('Compartilhado com sucesso'))
+      .catch((error) => console.log('Error sharing', error));
+
   } else {
     console.log("share api not found")
   }
